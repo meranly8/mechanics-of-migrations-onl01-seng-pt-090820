d39480b88bc0ca700269676286307b74efd61f9f
Michelle Ranly <meranly8@gmail.com>
2020-11-18 02:13:27 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,14 +15,14 @@ ActiveRecord::Base.establish_connection(
   :database => "db/artists.sqlite"
 )
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS  artists (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    genre TEXT,
-    age INTEGER,
-    hometown TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS  artists (
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     genre TEXT,
+#     age INTEGER,
+#     hometown TEXT
+#   )
+# SQL
+#
+# ActiveRecord::Base.connection.execute(sql)
